9ad19263c7b5ac1d4f10562e97b91aa92330504b
Walace Nascimento <wlacens@hotmail.com>
2025-01-27 21:09:57 -0300

    Implementando a função adicionar amigos, capturando o valor do input

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    console.log(nomeAmigo);
+}
+adicionarAmigo()
