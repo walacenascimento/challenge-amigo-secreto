b150ab13092a6571f2541ca8e4505772e0bb0705
Walace Nascimento <wlacens@hotmail.com>
2025-02-05 20:57:36 -0300

    Finaliando a função sortearAmig e inserindo comentários no código

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; // Array de amigos iniciado vazio
 
-let amigos = [];
-
-function adicionarAmigo() {
+function adicionarAmigo() { // Função que adiciona o nome do amigo 
     let nomeAmigo = document.querySelector('input').value;
     if (nomeAmigo == '') {
         alert("Por favor, insira um nome");
     } else {
         amigos.push(nomeAmigo);
-        console.log(nomeAmigo);
         limparCampo()
         listaDeAmigos()
-        
     }
 }
 
-function limparCampo() {
+function limparCampo() { // função que limpa o campo, após o nome ser inserido
     let limparCampo = document.querySelector('input');
     limparCampo.value = ''
 }
 
+// Função que captura o elemento de lista PAI (UL), cria um novo elemento filho (li), percorre o array amigo e adicona o nome do ao novo elemente filho.
 function listaDeAmigos() {
     let listaDeAmigos = document.getElementById('listaAmigos');listaDeAmigos.innerHTML = ' ';
     for (let index = 0; index < amigos.length; index++) {
@@ -30,11 +27,15 @@ function listaDeAmigos() {
 }
 
 function sortearAmigo() {
-    const sortearAmigo =document.getElementById('resultado');
-    const sorteado = document.createElement('li');
-    const sortudo = Math.floor(Math.random() * amigos.length);
-    const amigoSorteado = amigos[sortudo];
-    console.log(amigoSorteado);
-    sorteado.textContent = `O amigo sorteado é ${amigoSorteado}`;
-    sortearAmigo.appendChild(sorteado);
+    if (amigos == '') { // Verifica se o array amigos está vazio
+        alert(`Ainda não existe nenhum amigo adicionado na lista!`)
+    } else {
+        const sortudo = Math.floor(Math.random() * amigos.length); // Gera um indice aleátorio
+        const amigoSorteado = amigos[sortudo]; // obtem o indice gerado
+        const sortearAmigo =document.getElementById('resultado'); // captura o elemeno html
+        const sorteado = document.createElement('li'); // Cria um novo elemente de lista li no html
+        sorteado.textContent = `O amigo sorteado é ${amigoSorteado}`; // recebe o indice obtido no html
+        sortearAmigo.appendChild(sorteado); // Insere o indice sorteado no novo elemente(li) que fi criado.
+    }
+    limparCampo();
 }
