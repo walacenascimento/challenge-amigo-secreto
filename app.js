9bad564486b144c6f5ef12d96f37ff27afc19abd
Walace Nascimento <wlacens@hotmail.com>
2025-02-05 23:21:55 -0300

    Implementa a função desativaBotao

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-let amigos = []; // Array de amigos iniciado vazio
+let amigos = []; // Array de amigos iniciado vazio.
 
 function adicionarAmigo() { // Função que adiciona o nome do amigo 
-    let nomeAmigo = document.querySelector('input').value;
+    let nomeAmigo = document.getElementById('amigo').value;
     if (nomeAmigo == '') {
         alert("Por favor, insira um nome");
     } else {
         amigos.push(nomeAmigo);
-        limparCampo()
+        limparCampo('input');
         listaDeAmigos()
     }
 }
 
-function limparCampo() { // função que limpa o campo, após o nome ser inserido
-    let limparCampo = document.querySelector('input');
+function limparCampo(tag) { // função que limpa o campo, após o nome ser inserido
+    let limparCampo = document.querySelector(tag);
     limparCampo.value = ''
 }
 
-// Função que captura o elemento de lista PAI (UL), cria um novo elemento filho (li), percorre o array amigo e adicona o nome do ao novo elemente filho.
+// Função que captura o elemento de lista PAI (UL), cria um novo elemento filho (li), percorre o array amigo e adiciona o nome do amigo ao novo elemente filho (li).
 function listaDeAmigos() {
     let listaDeAmigos = document.getElementById('listaAmigos');listaDeAmigos.innerHTML = ' ';
     for (let index = 0; index < amigos.length; index++) {
@@ -35,7 +35,19 @@ function sortearAmigo() {
         const sortearAmigo =document.getElementById('resultado'); // captura o elemeno html
         const sorteado = document.createElement('li'); // Cria um novo elemente de lista li no html
         sorteado.textContent = `O amigo sorteado é ${amigoSorteado}`; // recebe o indice obtido no html
-        sortearAmigo.appendChild(sorteado); // Insere o indice sorteado no novo elemente(li) que fi criado.
+        sortearAmigo.appendChild(sorteado); // Insere o indice sorteado no novo elemente(li) que foi criado.
+        limparListaDeAmigo()
+        desativaarBotao()
     }
-    limparCampo();
+}
+
+// Função que limpa a lista de amigos, após o sorteio.
+function limparListaDeAmigo() {
+    const itens = document.getElementById('listaAmigos');
+    itens.innerHTML = '';
+}
+
+function desativaarBotao() {
+    const button = document.querySelectorAll('button')[1];
+    button.setAttribute('disabled', true);
 }
